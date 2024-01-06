77b1e9345bbc01f770cce08cdd0473e3f41f8a6f
PrivateForrestGump <forrestleung58@gmail.com>
2024-01-06 14:45:28 +0800

    im gay

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 Q1: 
 '''
 
-x = None    # TODO: Replace `None` with your answer
+x = 10    # TODO: Replace `None` with your answer
 
 print('Q1:')
 print(f'x = {x}')
@@ -13,9 +13,9 @@ print('#' * 30)
 Q2:
 '''
 
-a = None    # TODO: Replace `None` with your answer
-b = None    # TODO: Replace `None` with your answer
-c = None    # TODO: Replace `None` with your answer
+a = 6    # TODO: Replace `None` with your answer
+b = 9    # TODO: Replace `None` with your answer
+c = 15    # TODO: Replace `None` with your answer
 
 print('Q2:')
 print(f'a = {a}')
@@ -27,8 +27,8 @@ print('#' * 30)
 Q3:
 '''
 
-first = None    # TODO: Replace `None` with your answer
-second = None   # TODO: Replace `None` with your answer
+first = 19   # TODO: Replace `None` with your answer
+second = 8   # TODO: Replace `None` with your answer
 
 print('Q3:')
 print(f'first = {first}')
@@ -40,19 +40,54 @@ Q4:
 '''
 
 # TODO: Write your code here
-
+first = 8
+second = 19
+first += second
+first -= second
+first -= second
 ##################################################
 '''
 Q5:
 '''
 
 # TODO: Write your code here
+work_hours = 4 + 5 + 8 + 4
+hourly_salary = 8.75
+total_pay = work_hours * hourly_salary
+owed_taxes = total_pay * 0.2
+# Calculate pay at work based on hours worked each day
+print("My total hours worked:")
+print(work_hours)
 
+print("My hourly salary:")
+print("$" + hourly_salary)
+
+print("My total pay:")
+print(total_pay)
+
+print("My taxes owed:")  # 20% tax
+print(owed_taxes)
 ##################################################
 '''
 Q6:
 '''
 
 # TODO: Write your code here
+# This program computes the total amount owed for a meal,
+# assuming 8% tax and a 15% tip.
+subTotal = 38 + 40 + 30
+tax = subTotal * 0.08
+tip = subTotal * 0.15
+total = subTotal + tax + tip
+print("Subtotal:")
+print(subTotal)
 
+print("Tax:")
+print(tax)
+
+print("Tip:")
+print(tip)
+
+print("Total:")
+print(total)
 ##################################################
